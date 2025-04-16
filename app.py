37a7e8544f8b4e46fb52e2a19157d940c44e0680
Tanakataizou <rihikarii@gmail.com>
2025-04-16 22:26:14 +0900

    dataframe

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 st.write("# title")
 
-st.caption("注釈")
-st.line_chart(df)
+st.caption("注釈") 
+
+import pandas as pd
+df=pd.Dataframe(
+    {
+        "first column":[1,2,3,4],
+        "second column":[10,20,30,40],
+    }
+)
+st.write(df)
