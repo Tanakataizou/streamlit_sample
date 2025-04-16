56febe5281422fb5f4fa3511b8a4fe1d4c9df8d4
Tanakataizou <rihikarii@gmail.com>
2025-04-16 22:54:53 +0900

    write

--- app.py
+++ app.py
@@ -17,3 +17,6 @@ st.image("https://i3.gamebiz.jp/media/0af4eaca-9e9e-433f-b815-4a097e3836f1.jpg")
 name=st.text_input("なまえ")
 
 age=st.number_input("年齢",step=1)
+
+st.write(f"名前:{name}")
+st.write(f"年齢:{age}")
