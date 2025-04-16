7f626b78f4acb13f47f1c586614e6d7d0ed97f79
Tanakataizou <rihikarii@gmail.com>
2025-04-16 22:34:55 +0900

    a

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 df=pd.DataFrame(
     {
         "first column":[1,2,3,4],
-        "second column":[10,20,30,40],
+        "second column":[10,20,30,20],
     }
 )
 st.write(df)
